7b1edc5722e31f091ad9f807283979badb021031
Serhii <mashchenko.serhii@lll.kpi.ua>
2025-05-18 01:28:16 +0300

    Update pz_3_3.c

--- pz_3/pz_3_3.c
+++ pz_3/pz_3_3.c
@@ -1,38 +1,55 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include <signal.h>
-#include <sys/resource.h>
 #include <unistd.h>
+#include <sys/resource.h>
 
-void handle_file_limit(int sig) {
-    printf("Перевищено обмеження на розмір файлу\n");
-    exit(1);
+#define MAX_CPU_TIME 2
+
+void set_cpu_limit() {
+    struct rlimit limit;
+    limit.rlim_cur = MAX_CPU_TIME;
+    limit.rlim_max = MAX_CPU_TIME;
+    
+    if (setrlimit(RLIMIT_CPU, &limit) == -1) {
+        perror("Помилка встановлення обмеження CPU");
+        exit(EXIT_FAILURE);
+    }
 }
 
-void set_file_limit(rlim_t size) {
-    struct rlimit lim = {size, size};
-    setrlimit(RLIMIT_FSIZE, &lim);
+void select_numbers(int *numbers, int count, int max) {
+    int selected[max + 1];
+    for (int i = 0; i <= max; i++) selected[i] = 0;
+    
+    for (int i = 0; i < count; ) {
+        int num = rand() % max + 1;
+        if (!selected[num]) {
+            selected[num] = 1;
+            numbers[i++] = num;
+        }
+    }
 }
 
 int main() {
-    signal(SIGXFSZ, handle_file_limit);
-    set_file_limit(3);  
-
-    FILE *f = fopen("dice.txt", "w");
-    if (!f) {
-        perror("fopen");
-        exit(1);
-    }
-
+    set_cpu_limit();
+    
     srand(time(NULL));
-    for (int i = 0; i < 1000; i++) {
-        int roll = rand() % 6 + 1;
-        fprintf(f, "Кидок %d: %d\n", i+1, roll);
-        fflush(f);
-        usleep(10000); 
-    }
-
-    fclose(f);
-    return 0;
+    
+    printf("Лотерейний симулятор (обмеження CPU: %d сек)\n", MAX_CPU_TIME);
+    printf("----------------------------------------\n");
+    
+    int main_numbers[7], bonus_numbers[6];
+    
+    select_numbers(main_numbers, 7, 49);
+    select_numbers(bonus_numbers, 6, 36);
+    
+    printf("7 з 49: ");
+    for (int i = 0; i < 7; i++) printf("%d ", main_numbers[i]);
+    
+    printf("\n6 з 36: ");
+    for (int i = 0; i < 6; i++) printf("%d ", bonus_numbers[i]);
+    
+    printf("Готово! Час CPU не перевищено.\n");
+    
+    return EXIT_SUCCESS;
 }
